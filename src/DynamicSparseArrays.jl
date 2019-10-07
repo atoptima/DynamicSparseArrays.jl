4f013b01a70c3e675a351836b8dc3fd79b1c4cdd
Guillaume Marques <guillaume.marques@protonmail.com>
2019-10-07 15:27:15 -0300

    keys

--- src/DynamicSparseArrays.jl
+++ src/DynamicSparseArrays.jl
@@ -2,7 +2,7 @@ module DynamicSparseArrays
 
 export PackedMemoryArray
 
-mutable struct PackedMemoryArray{T}
+mutable struct PackedMemoryArray{K,T}
     capacity::Int
     segment_capacity::Int
     nb_segments::Int
@@ -12,18 +12,17 @@ mutable struct PackedMemoryArray{T}
     t_0::Float64 # upper density treshold at leaves
     p_h::Float64 # lower density treshold at root
     p_0::Float64 # lower density treshold at leaves
-    exist::Vector{Bool}
-    array::Vector{T}
+    array::Vector{Tuple{K,T}}
 end
 
-function PackedMemoryArray{T}(capacity::Int) where {T}
+function PackedMemoryArray{K,T}(capacity::Int) where {K,T}
     seg_capacity = Int(ceil(log2(capacity)))
     nb_segs = Int(ceil(capacity / seg_capacity))
     height = Int(ceil(log2(nb_segs)))
     real_capacity = nb_segs * seg_capacity
     return PackedMemoryArray{T}(
         real_capacity, seg_capacity, nb_segs, 0, height, 1.0, 0.75, 0.5, 0.25, 
-        Vector{T}(undef, capacity), zeros(Bool, capacity)
+        Vector{Tuple{K,T}}(undef, capacity)
     )
 end
 
