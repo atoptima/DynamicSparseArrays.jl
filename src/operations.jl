0d47cf4f50d3bd3698b13dc52c26e17feed5e327
Guillaume Marques <guillaume.marques@protonmail.com>
2021-03-21 21:07:05 +0100

    add support of spMv

---
+++ src/operations.jl
@@ -0,0 +1,56 @@
+struct Transposed{T}
+    array::T
+end
+
+Base.transpose(M::DynamicSparseMatrix) = Transposed(M)
+
+function Base.:(*)(M::DynamicSparseMatrix{K,L,T}, v::PackedMemoryArray{L,T}) where {K,L,T}
+    return _mult(M.colmajor, v)
+end
+
+function Base.:(*)(M::Transposed{DynamicSparseMatrix{K,L,T}}, v::PackedMemoryArray{K,T}) where {K,L,T}
+    return _mult(M.array.rowmajor, v)
+end
+
+function _mult(M::MappedPackedCSC{K,L,T}, v::PackedMemoryArray{L,T}) where {K,L,T}
+    result = Dict{L, T}()
+
+    col_key_pos::Int = 1
+    next_col_key_pos::Int = 2
+
+    row_start = 0
+    row_end = 0
+    row_pos = 0
+
+    for vec_pos in 1:length(v.array)
+        entry = v.array[vec_pos]
+        if entry !== nothing
+            row_id, val = entry
+            while col_key_pos <= length(M.col_keys) && M.col_keys[col_key_pos] != row_id
+                col_key_pos += 1
+            end
+            (col_key_pos > length(M.col_keys)) && break
+            next_col_key_pos = col_key_pos + 1
+            while next_col_key_pos <= length(M.col_keys) && M.col_keys[next_col_key_pos] === nothing
+                next_col_key_pos += 1
+            end
+
+            row_start = M.pcsc.semaphores[col_key_pos] + 1
+            row_end = length(M.pcsc.pma.array)
+            if next_col_key_pos <= length(M.col_keys)
+                row_end = M.pcsc.semaphores[next_col_key_pos] - 1
+            end
+
+            for row_pos in row_start:row_end
+                entry = M.pcsc.pma.array[row_pos]
+                if entry !== nothing
+                    matrix_col_id, coeff = entry
+                    result[matrix_col_id] = get(result, matrix_col_id, 0.0) + val * coeff
+                end
+            end
+            col_key_pos = next_col_key_pos
+        end
+    end
+    return result
+end
+
---
+++ test/unit/spmv.jl
@@ -0,0 +1,47 @@
+DynamicSparseArrays.semaphore_key(::Type{Float64}) = 0.0
+
+function test_spmv_1()
+    I = [1.0, 1.0, 1.0, 2.0, 2.0, 3.0, 4.0, 4.0, 4.0]
+    J = [1, 3, 5, 2, 4, 4, 1, 4, 5]
+    V = [1, 2, 1, 2, 1, 3, 3, 2, 2]
+    matrix = dynamicsparse(I, J, V)
+
+    I2 = [1, 3, 5]
+    V2 = [1, 1, 1]
+    vec = dynamicsparsevec(I2, V2)
+
+    result = matrix * vec
+
+    # The multiplication returns a Dict.
+    @test result[1.0] == 4
+    @test get(result, 2.0, 0.0) == 0.0
+    @test get(result, 3.0, 0.0) == 0.0
+    @test result[4.0] == 5
+    @test get(result, 5.0, 0.0) == 0.0
+end
+
+
+function test_spmv_2()
+    I = [1, 1, 1, 2, 2, 3, 4, 4, 4]
+    J = [1.0, 3.0, 5.0, 2.0, 4.0, 4.0, 1.0, 4.0, 5.0]
+    V = [1, 2, 1, 2, 1, 3, 3, 2, 2]
+    matrix = dynamicsparse(I, J, V)
+
+    I2 = [1, 3, 5]
+    V2 = [1, 1, 1]
+    vec = dynamicsparsevec(I2, V2)
+
+    result = transpose(matrix) * vec
+
+    # The multiplication returns a Dict.
+    @test result[1.0] == 1
+    @test get(result, 2, 0.0) == 0.0
+    @test result[3.0] == 2
+    @test result[4.0] == 3
+    @test result[5.0] == 1
+end
+
+function test_spmv()
+    test_spmv_1()
+    test_spmv_2()
+end
